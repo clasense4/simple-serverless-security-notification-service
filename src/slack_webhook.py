027fc1f9dd29455e24668916450e4187701cfea8
Fajri Abdillah <clasense4@gmail.com>
2020-06-20 20:41:51 +0700

    Update slack message webhook

--- src/slack_webhook.py
+++ src/slack_webhook.py
@@ -3,6 +3,42 @@ import json
 import requests
 import jmespath
 
+def send_slack_message(event_type, json_message):
+    # https://gist.github.com/devStepsize/b1b795309a217d24566dcc0ad136f784
+    webhook_url = os.environ['WEBHOOK_URL']
+
+    slack_data = {
+        "username": "securitybot",
+        "icon_emoji": "ghost",
+        "text": "New security event",
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "New security event : *" + event_type + "* :warning: @here"
+                }
+            },
+            {
+                "type": "section",
+                "block_id": "section567",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "```" + json_message + "```"
+                },
+            }
+        ]
+    }
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
+
 events = {
     1 : 'events/ssh-open-to-world.json',
     2 : 'events/port-8080-open-to-world.json',
@@ -12,7 +48,6 @@ events = {
 
 with open(events[4]) as f:
     event = json.load(f)
-# print(event)
 
 queries = {
     'ssh-open-to-world':'requestParameters.ipPermissions.items[?ipProtocol == `tcp` && fromPort == `22` && toPort == `22` && ipRanges.items[?cidrIp==`0.0.0.0/0`]]',
@@ -25,21 +60,4 @@ for query in queries:
     path = jmespath.search(queries[query], event)
     if path:
         print(query + " is found!")
-
-# https://gist.github.com/devStepsize/b1b795309a217d24566dcc0ad136f784
-# webhook_url = os.environ['WEBHOOK_URL']
-# slack_data = {
-#     'username': 'securitybot',
-#     'icon_emoji': 'ghost',
-#     'text': "This is a security message :warning: <!here>"
-# }
-
-# response = requests.post(
-#     webhook_url, data=json.dumps(slack_data),
-#     headers={'Content-Type': 'application/json'}
-# )
-# if response.status_code != 200:
-#     raise ValueError(
-#         'Request to slack returned an error %s, the response is:\n%s'
-#         % (response.status_code, response.text)
-#     )
+        send_slack_message(query, json.dumps(event, indent=4))
