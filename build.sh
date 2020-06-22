853d69ec0bc8d0dd396f1c5b567c21ca853f0340
Fajri Abdillah <clasense4@gmail.com>
2020-06-22 17:11:09 +0700

    Remove unused operation

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ set -eo pipefail
 rm -rf .venv package layer.zip lambda_package.zip
 virtualenv -p `which python3` .venv
 source .venv/bin/activate
-rm -rf package
 cd src
 pip install --target ../package/python -r requirements.txt
 
